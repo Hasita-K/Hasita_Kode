61890a933d3d68f0892e46cc2d05cbffff525d0c
Hasita-K <kodehasita@gmail.com>
2026-01-30 11:53:20 -0500

    added another program

---
+++ src/intro_to_c_projects/smallprogram0_Kode_Hasita.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("The current month is August. \n");
+	printf("The current year is 2025. \n");
+	printf("I am a student at the University of Central Florida. \n");
+	printf("This Fall 2025, I am taking COP3223 Introduction to Programming with C with Dr. Steinberg. \n");
+	printf("I am super excited to learn the C Language this semester! \n");
+	printf("Marvel Studios released Fantastic Four First Steps this past Summer! \n");
+	printf("It was an AWESOME movie! \n");
+	
+	return 0;
+}
